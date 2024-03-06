f78eff0e33896c7cfb0045917667d3c8ed6ad973
André Machado <andrels.machado82@gmail.com>
2024-03-05 22:17:33 -0300

    Criando MS de notificação SNS da proposta

---
+++ src/main/java/com/andre/notificacao/constantes/MensagemConstante.java
@@ -0,0 +1,7 @@
+package com.andre.notificacao.constantes;
+
+public class MensagemConstante {
+
+    public static final String PROPOSTA_EM_ANALISE =
+            "Prezado(a) %s, sua proposta foi recebida por nosssa equipe e será analisada. Em breve retornaremos o contato.";
+}
---
+++ src/main/java/com/andre/notificacao/listener/PropostaPendenteListener.java
@@ -0,0 +1,22 @@
+package com.andre.notificacao.listener;
+
+import com.andre.notificacao.constantes.MensagemConstante;
+import com.andre.notificacao.domain.Proposta;
+import com.andre.notificacao.service.NotificacaoSnsService;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropostaPendenteListener {
+
+    @Autowired
+    private NotificacaoSnsService notificacaoSnsService;
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void propostaPendente(Proposta proposta) {
+        String mensagem =
+                String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+        notificacaoSnsService.notificar(proposta.getUsuario().getTelefone(),mensagem);
+    }
+}
